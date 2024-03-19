608c3bf90be1568bb6f66d33add7909022627b8c
unknown <dassudeshna96@gmail.com>
2024-03-19 12:13:16 +0530

    added two toplevel window

--- hotel-management-site-using-tkinter.py
+++ hotel-management-site-using-tkinter.py
@@ -1,11 +1,65 @@
 import tkinter
+import tkinter as tk
 from tkinter import *
 from tkinter import PhotoImage
+from tkinter import ttk
+def create_win():
+    extra_win = tk.Toplevel()
+    extra_win.title('order page')
+    extra_win.geometry("300x300")
+    extra_win.resizable(False,False)
+
+    Label(extra_win, text = "Thankyou for ordering", font = ('Georgia', 15)).place(x = 55, y = 150)
+
+    
+def create_window():
+    window = tk.Toplevel()
+    window.title('Our menu')
+    window.geometry("400x400")
+    window.resizable(False,False)
+     
+    Label(window, text = 'Dishes', font = ('Georgia',20,"bold")).place(x = 150, y = 50)
+    Label(window, text = 'Breakfast', font = ('Georgia',10)).place(x = 30, y = 100)
+    cb3 = IntVar()
+    cb4 = IntVar()
+    cb5 = IntVar()
+    Button4 = Checkbutton(window, text = "bread-Egg-fruitjuice",variable = cb3, onvalue = 1, offvalue = 0,height = 1)
+    Button4.place(x = 110, y = 100)
+    Button5 = Checkbutton(window, text = "Idli/Dosa",variable = cb4, onvalue = 1, offvalue = 0,height = 1)
+    Button5.place(x = 240, y = 100)
+    Button6 = Checkbutton(window, text = "Poha/Upma",variable = cb5, onvalue = 1, offvalue = 0,height = 1)
+    Button6.place(x = 309, y = 100)
+    
+    Label(window, text = 'Lunch', font = ('Georgia',10)).place(x = 30, y = 150)
+    cb6 = IntVar()
+    cb7 = IntVar()
+    Button7 = Checkbutton(window, text = "NorthIndian-Thali",variable = cb6, onvalue = 1, offvalue = 0,height = 1)
+    Button7.place(x = 110, y = 150)
+    Button8 = Checkbutton(window, text = "SouthIndian-Thali",variable = cb7, onvalue = 1, offvalue = 0,height = 1)
+    Button8.place(x = 250, y = 150)
+
+    Label(window, text = 'Dinner', font = ('Georgia',10)).place(x = 30, y = 200)
+    cb8 = IntVar()
+    cb9 = IntVar()
+    Button9 = Checkbutton(window, text = "Italian/Chinese",variable = cb8, onvalue = 1, offvalue = 0,height = 1)
+    Button9.place(x = 110, y = 200)
+    Button10 = Checkbutton(window, text = "Indian cuisine",variable = cb9, onvalue = 1, offvalue = 0,height = 1)
+    Button10.place(x = 250, y = 200)
+
+    Button11 = Button(window, text = "Confirm your order", command = create_win)
+    Button11.place(x = 150, y = 300)
+    
+    
+    
+
+          
+                   
 win = tkinter.Tk()
 win.title("Hotel Management")
 win.config(bg="white")
 win.geometry("500x600")
 win.resizable(False,False)
+
 image_path = PhotoImage(file=r"C:\Users\dassu\OneDrive\Desktop\workspace\python-tkinter-learning\images\welcome-image.png")
 bg_image = Label(win,image = image_path)
 bg_image.place(relheight = 1, relwidth = 1)
@@ -16,12 +70,13 @@ service_lbl = Label(win, text = "Services available", bg = "white", fg = "black"
 service_lbl.place(x = 30, y = 100)
 cb1 = IntVar()
 cb2 = IntVar()
-Button1 = Checkbutton(win, text = "Roomservice",  justify = "left",variable = cb1, onvalue = 1, offvalue = 0,height = 1,width = 4)
-Button2 = Checkbutton(win, text = "Dine-In",  justify = "right",variable = cb2, onvalue = 1, offvalue = 0,height = 1,width = 4)
+Button1 = Checkbutton(win, text = "Roomservice",variable = cb1, onvalue = 1, offvalue = 0,height = 1)
+Button2 = Checkbutton(win, text = "Dine-In",variable = cb2, onvalue = 1, offvalue = 0,height = 1)
 Button1.place(x = 200, y = 100)
 Button2.place(x = 300, y = 100)
 menu_lbl = Label(win, text = "Choose exotic dishes from our curated menu!!!", bg = "white", fg = "black", justify = "left", font = ("Georgia",16))
 menu_lbl.place(x = 30, y = 200)
-Button3 = Button(win, text = "Click here",justify = "center")
+Button3 = Button(win, text = "Click here", command = create_window)
 Button3.place(x = 200, y = 250)
+
 win.mainloop()
